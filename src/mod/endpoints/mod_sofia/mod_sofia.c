f0fe19a05f6b060e8fa72124bee64756d4c08504
Alan Evans <alan@kagesys.com>
2014-01-30 19:20:18 +0000

    Add support for from and to fields in SEND_MESSAGE handling

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -5581,8 +5581,10 @@ static void general_event_handler(switch_event_t *event)
 		{
 			const char *profile_name = switch_event_get_header(event, "profile");
 			const char *ct = switch_event_get_header(event, "content-type");
-			const char *user = switch_event_get_header(event, "user");
-			const char *host = switch_event_get_header(event, "host");
+			char *user = switch_event_get_header(event, "user");
+			char *host = switch_event_get_header(event, "host");
+			const char *to = switch_event_get_header(event, "to");
+			const char *from = switch_event_get_header(event, "from");
 			const char *subject = switch_event_get_header(event, "subject");
 			const char *uuid = switch_event_get_header(event, "uuid");
 			const char *body = switch_event_get_body(event);
@@ -5595,7 +5597,55 @@ static void general_event_handler(switch_event_t *event)
 				return;
 			}
 
-			if (ct && user && host) {
+/* AlanE: Added this 'if' to attempt to use to and from fields if provided in the SEND_MESSAGE Event */
+            if (ct && to && from) {
+                char *contact, *p;
+                switch_console_callback_match_t *list = NULL;
+                switch_console_callback_match_node_t *m;
+				char *from_id = NULL, *to_id = NULL;
+
+                char *id = strdup(to);
+
+                switch_assert(id);
+
+                if ((host = strchr(id, '@'))) {
+                      *host++ = '\0';
+                      user = id;
+                } else {
+                      host = id;
+                }
+
+                if (!(list = sofia_reg_find_reg_url_multi(profile, user, host))) {
+                    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Can't find registered user %s@%s\n", user, host);
+                    return;
+                }
+
+				from_id = switch_mprintf("sip:%s", from);
+				to_id = switch_mprintf("sip:%s", to);
+
+                for (m = list->head; m; m = m->next) {
+                    contact = sofia_glue_get_url_from_contact(m->val, 0);
+
+                    if ((p = strstr(contact, ";fs_"))) {
+                        *p = '\0';
+                    }
+
+                    nh = nua_handle(profile->nua,
+                                    NULL, NUTAG_URL(contact), SIPTAG_FROM_STR(from_id), SIPTAG_TO_STR(to_id), SIPTAG_CONTACT_STR(profile->url), TAG_END());
+
+                    nua_message(nh, NUTAG_NEWSUB(1), SIPTAG_CONTENT_TYPE_STR(ct),
+                                TAG_IF(!zstr(body), SIPTAG_PAYLOAD_STR(body)), TAG_IF(!zstr(subject), SIPTAG_SUBJECT_STR(subject)), TAG_END());
+                }
+
+                free(id);
+                free(from_id);
+                free(to_id);
+                switch_console_free_matches(&list);
+
+                sofia_glue_release_profile(profile);
+
+/* AlanE: Original 'if' used user and host for both to and from fields - retained. just in case, for backward compat */
+			} else if (ct && user && host) {
 				char *id = NULL;
 				char *contact, *p;
 				switch_console_callback_match_t *list = NULL;
@@ -5618,7 +5668,7 @@ static void general_event_handler(switch_event_t *event)
 					}
 
 					nh = nua_handle(profile->nua,
-									NULL, NUTAG_URL(contact), SIPTAG_FROM_STR(id), SIPTAG_TO_STR(id), SIPTAG_CONTACT_STR(profile->url), TAG_END());
+									NULL, NUTAG_URL(contact), SIPTAG_FROM_STR(from), SIPTAG_TO_STR(to), SIPTAG_CONTACT_STR(profile->url), TAG_END());
 
 					nua_message(nh, NUTAG_NEWSUB(1), SIPTAG_CONTENT_TYPE_STR(ct),
 								TAG_IF(!zstr(body), SIPTAG_PAYLOAD_STR(body)), TAG_IF(!zstr(subject), SIPTAG_SUBJECT_STR(subject)), TAG_END());
